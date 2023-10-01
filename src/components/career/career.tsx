a08f13512995dcb42f8329a1a49f9a0e0127f62b
m02-253 <mossan0917@gmail.com>
2023-10-01 22:54:31 +0900

    Start to rewrite motion.div to useAnimate

--- src/components/career/career.tsx
+++ src/components/career/career.tsx
@@ -1,43 +1,83 @@
-import { JSX } from 'react';
+import { JSX, useCallback, useRef, useState } from 'react';
 import { Box, Center, Text } from '@chakra-ui/react';
 import niigataPref from '../../public/prefectures/niigata.png';
 import { HandyNextImage } from '@/components/images/handyNextImage';
 import { ImHere } from '@/components/career/movementHistory/imHere';
 
+type XYCoordinates = Record<'x' | 'y', number>;
+const shibataCoordinates: XYCoordinates = { x: 200, y: 90 };
+const nagaokaCoordinates: XYCoordinates = { x: 150, y: 170 };
+const niigataCoordinates: XYCoordinates = { x: 180, y: 100 };
+
 export const Career = (): JSX.Element => {
   const imageSize = '18rem'; // 新潟県画像の縦幅かつ横幅
-  return (
-    <Center w={'100vw'} h={'100vh'} flexDirection={'row'}>
-      <Box position={'relative'} w={imageSize} h={imageSize}>
-        <ImHere />
-        <HandyNextImage
-          boxProps={{
-            width: imageSize,
-            height: imageSize,
-            bgColor: 'yellow',
-          }}
-          imageProps={{ src: niigataPref, alt: 'niigata-pref' }}
-        />
-      </Box>
 
-      <Box
-        w={`calc(100% - ${imageSize})`}
-        overflowY={'scroll'}
-        scrollBehavior={'smooth'}
-        h={imageSize}
-        scrollSnapType={'y mandatory'}
-        bgColor={'cyan'}
-      >
-        <Text h={imageSize} scrollSnapAlign={'start'}>
-          経歴1
-        </Text>
-        <Text h={imageSize} scrollSnapAlign={'start'}>
-          経歴2
-        </Text>
-        <Text h={imageSize} scrollSnapAlign={'start'}>
-          経歴3
-        </Text>
-      </Box>
+  // スクロールされたとき、記事に対応する座標を変更する
+  const [mapCoordinates, setMapCoordinates] =
+    useState<XYCoordinates>(shibataCoordinates);
+
+  const careerBoxRef = useRef<HTMLDivElement>(null);
+  const handleScroll = useCallback(() => {
+    if (!careerBoxRef.current) return;
+    const { scrollTop, clientHeight } = careerBoxRef.current;
+    const seenIndex = Math.floor(scrollTop / clientHeight);
+    switch (seenIndex) {
+      case 0:
+        setMapCoordinates(shibataCoordinates);
+        break;
+      case 1:
+        setMapCoordinates(nagaokaCoordinates);
+        break;
+      case 2:
+        setMapCoordinates(nagaokaCoordinates);
+        break;
+      case 3:
+        setMapCoordinates(niigataCoordinates);
+        break;
+      default:
+        return;
+    }
+  }, []);
+
+  return (
+    <Center>
+      <Center w={'min(100vw, 68rem)'} h={'100vh'} flexDirection={'row'}>
+        <Box position={'relative'} w={imageSize} h={imageSize}>
+          <ImHere coordinates={mapCoordinates} />
+          {/*<ImHere coordinates={{ x: 150, y: 170 }} />*/}
+          <HandyNextImage
+            boxProps={{
+              width: imageSize,
+              height: imageSize,
+            }}
+            imageProps={{ src: niigataPref, alt: 'niigata-pref' }}
+          />
+        </Box>
+
+        <Box
+          w={`calc(100% - ${imageSize})`}
+          overflowY={'scroll'}
+          scrollBehavior={'smooth'}
+          h={imageSize}
+          scrollSnapType={'y mandatory'}
+          gap={'1px'}
+          onScroll={handleScroll}
+          ref={careerBoxRef}
+        >
+          <Text h={imageSize} scrollSnapAlign={'start'}>
+            中学時代まで新発田で過ごす
+          </Text>
+          <Text h={imageSize} scrollSnapAlign={'start'}>
+            長岡で高専、大学に行く
+          </Text>
+          <Text h={imageSize} scrollSnapAlign={'start'}>
+            大学院時代にITスタートアップの共同創業者となる
+          </Text>
+          <Text h={imageSize} scrollSnapAlign={'start'}>
+            大学院時代に共同創業した会社を新潟市に移し、通勤するようになる
+          </Text>
+        </Box>
+      </Center>
     </Center>
   );
 };
--- src/components/career/movementHistory/imHere.tsx
+++ src/components/career/movementHistory/imHere.tsx
@@ -1,33 +1,68 @@
-import { JSX } from 'react';
+import { JSX, useMemo } from 'react';
 import { Box } from '@chakra-ui/react';
 import { motion } from 'framer-motion';
 
-export const ImHere = (): JSX.Element => {
+type Props = {
+  coordinates: { x: number; y: number };
+};
+
+export const ImHere = (props: Props): JSX.Element => {
+  const pointSize = 10;
+  const rippleSpreadTo = 30;
+
+  type TopLeft = Record<
+    'beforeTop' | 'beforeLeft' | 'afterTop' | 'afterLeft',
+    number
+  >;
+  const topLeft = useMemo<TopLeft>(() => {
+    const afterOffset = -(rippleSpreadTo - pointSize) / 2; // 拡大に伴い、この分だけずれる
+    return {
+      beforeTop: props.coordinates.y,
+      beforeLeft: props.coordinates.x,
+      afterTop: props.coordinates.y + afterOffset,
+      afterLeft: props.coordinates.x + afterOffset,
+    };
+  }, [props.coordinates.x, props.coordinates.y]);
+
   return (
     <Box position={'relative'}>
       {/* 真ん中の赤点 */}
       <Box
         position={'absolute'}
         bgColor={'red'}
-        w={'10px'}
-        h={'10px'}
+        w={`${pointSize}px`}
+        h={`${pointSize}px`}
         borderRadius={'full'}
         zIndex={1}
+        top={topLeft.beforeTop}
+        left={topLeft.beforeLeft}
       />
 
       {/* 広がっていく赤線 */}
       <motion.div
         style={{
           position: 'absolute',
-          borderRadius: '1000rem',
-          borderWidth: '1px',
+          borderRadius: rippleSpreadTo,
+          borderWidth: '2px',
           borderColor: 'red',
           zIndex: 2,
         }}
-        initial={{ opacity: 1, width: '10px', height: '10px' }}
-        animate={{ opacity: 0, width: '30px', height: '30px' }}
+        initial={{
+          opacity: 1,
+          width: pointSize,
+          height: pointSize,
+          top: `${topLeft.beforeTop}px`,
+          left: `${topLeft.beforeLeft}px`,
+        }}
+        animate={{
+          opacity: 0,
+          width: rippleSpreadTo,
+          height: rippleSpreadTo,
+          top: `${topLeft.afterTop}px`,
+          left: `${topLeft.afterLeft}px`,
+        }}
         transition={{ repeat: Infinity, duration: 2 }}
-      ></motion.div>
+      />
     </Box>
   );
 };
