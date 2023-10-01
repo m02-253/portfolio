09f96938957a0ad16720f9281d5637e12905b94f
m02-253 <mossan0917@gmail.com>
2023-10-02 00:13:38 +0900

    Implement Career component logic

--- src/components/career/movementHistory/imHere.tsx
+++ src/components/career/movementHistory/imHere.tsx
@@ -1,15 +1,18 @@
-import { JSX, useMemo } from 'react';
+import { JSX, useMemo, useRef } from 'react';
 import { Box } from '@chakra-ui/react';
-import { motion } from 'framer-motion';
+import { useAnimationFrame } from 'framer-motion';
 
 type Props = {
   coordinates: { x: number; y: number };
 };
 
 export const ImHere = (props: Props): JSX.Element => {
+  // 定数
+  const animationDurationSec = 1.5;
   const pointSize = 10;
   const rippleSpreadTo = 30;
 
+  // リプルの位置
   type TopLeft = Record<
     'beforeTop' | 'beforeLeft' | 'afterTop' | 'afterLeft',
     number
@@ -23,6 +26,30 @@ export const ImHere = (props: Props): JSX.Element => {
       afterLeft: props.coordinates.x + afterOffset,
     };
   }, [props.coordinates.x, props.coordinates.y]);
+
+  // リプルのアニメーション
+  const rippleBoxRef = useRef<HTMLDivElement>(null);
+  useAnimationFrame((time) => {
+    if (!rippleBoxRef.current) return;
+    // 初期状態のときの値にbeforeFactorをかけて、
+    // 完了状態のときの値にafterFactorをかけて、それらの和を取ると、time[ms]時点での値になる。
+    const afterFactor = (time / 1000 / animationDurationSec) % 1;
+    const beforeFactor = 1 - afterFactor;
+
+    const size = beforeFactor * pointSize + afterFactor * rippleSpreadTo;
+    rippleBoxRef.current.style.width = `${size}px`;
+    rippleBoxRef.current.style.height = `${size}px`;
+
+    const top =
+      beforeFactor * topLeft.beforeTop + afterFactor * topLeft.afterTop;
+    const left =
+      beforeFactor * topLeft.beforeLeft + afterFactor * topLeft.afterLeft;
+    rippleBoxRef.current.style.top = `${top}px`;
+    rippleBoxRef.current.style.left = `${left}px`;
+
+    const opacity = beforeFactor.toString();
+    rippleBoxRef.current.style.opacity = opacity;
+  });
 
   return (
     <Box position={'relative'}>
@@ -39,29 +66,18 @@ export const ImHere = (props: Props): JSX.Element => {
       />
 
       {/* 広がっていく赤線 */}
-      <motion.div
-        style={{
-          position: 'absolute',
-          borderRadius: rippleSpreadTo,
-          borderWidth: '2px',
-          borderColor: 'red',
-          zIndex: 2,
-        }}
-        initial={{
-          opacity: 1,
-          width: pointSize,
-          height: pointSize,
-          top: `${topLeft.beforeTop}px`,
-          left: `${topLeft.beforeLeft}px`,
-        }}
-        animate={{
-          opacity: 0,
-          width: rippleSpreadTo,
-          height: rippleSpreadTo,
-          top: `${topLeft.afterTop}px`,
-          left: `${topLeft.afterLeft}px`,
-        }}
-        transition={{ repeat: Infinity, duration: 2 }}
+      <Box
+        ref={rippleBoxRef}
+        position={'absolute'}
+        borderRadius={'full'}
+        borderWidth={'2px'}
+        borderColor={'red'}
+        opacity={1}
+        width={`${pointSize}px`}
+        height={`${pointSize}px`}
+        top={`${topLeft.beforeTop}px`}
+        left={`${topLeft.beforeLeft}px`}
+        zIndex={2}
       />
     </Box>
   );
